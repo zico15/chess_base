42a3c95fb91b0e4e3825d1dd7e64e71946bcfa22
ezequiel <55319859+zico15@users.noreply.github.com>
2021-04-02 21:50:33 +0100

    zico

--- index.js
+++ index.js
@@ -1,76 +1,25 @@
-// Setup basic express server
-const express = require('express');
-const app = express();
-const path = require('path');
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const port = process.env.PORT || 3000;
+const WebSocket = require('ws');
 
-server.listen(port, () => {
-  console.log('Server listening at port %d', port);
-});
+let port = process.env.PORT || 3000
 
-// Routing
-app.use(express.static(path.join(__dirname, 'public')));
+const server = new WebSocket.Server({ port });
 
-// Chatroom
+let sockets = [];
+server.on('connection', function(socket) {
+  // Adicionamos cada nova conexão/socket ao array `sockets`
+  sockets.push(socket);
 
-let numUsers = 0;
-
-io.on('connection', (socket) => {
-  let addedUser = false;
-  console.log('conectato');
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', (data) => {
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
+  // Quando você receber uma mensagem, enviamos ela para todos os sockets
+  socket.on('message', function(msg) {
+      console.log(msg);
+    sockets.forEach(s => s.send(msg));
   });
 
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', (username) => {
-    if (addedUser) return;
-
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', () => {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', () => {
-    if (addedUser) {
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
+  // Quando a conexão de um socket é fechada/disconectada, removemos o socket do array
+  socket.on('close', function() {
+      
+    console.log('client :'+ sockets.length);
+    sockets = sockets.filter(s => s !== socket);
+    console.log('client close:'+ sockets.length);
   });
 });
