9d7a3c396aa9ab4649df84c668afcd62d32f734a
ezequiel <55319859+zico15@users.noreply.github.com>
2021-04-03 12:38:21 +0100

    Update serve.js

--- serve.js
+++ serve.js
@@ -1,21 +1,5 @@
 let port = process.env.PORT || 3000
 
-/*const http = require('http');
-
-
-
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, () => {
-  console.log(`Server running at`);
-});
-
-*/
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -25,6 +9,28 @@ var clients = {};
 app.get('/', function(req, res){
 res.send('server is running');
 });
+
+
+io.on("connection", function (client) {
+    console.log("client: " + client);
+    client.on("join", function(name){
+      console.log("Joined: " + name);
+      clients[client.id] = name;
+      client.emit("update", "You have connected to the server.");
+      client.broadcast.emit("update", name + " has joined the server.")
+    });
+  
+    client.on("send", function(msg){
+      console.log("Message: " + msg);
+      client.broadcast.emit("chat", clients[client.id], msg);
+    });
+  
+    client.on("disconnect", function(){
+      console.log("Disconnect");
+      io.emit("update", clients[client.id] + " has left the server.");
+      delete clients[client.id];
+    });
+  });
 
 //SocketIO vem aqui
 
